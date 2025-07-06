2e1a8e2ba790ba036b705cb1b0aa4f6a89ca65c8
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-07-06 23:47:43 +1000

    minor update
    minor update

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -25,8 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   
   document.addEventListener("DOMContentLoaded", function () {
-    // EmailJS 초기화 - 반드시 실제 Public Key로
-    emailjs.init("AJ9VRnG1JVoy76hBI"); // ✅ 실제 Public Key 입력
+    emailjs.init("AJ9VRnG1JVoy76hBI"); 
   
     // 오늘 날짜 이후만 선택 가능하도록 설정
     const today = new Date().toISOString().split("T")[0];
@@ -44,14 +43,13 @@ document.addEventListener('DOMContentLoaded', function () {
       const emailValue = emailInput.value.trim();
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
-      // 이메일 형식 확인
+
       if (!emailRegex.test(emailValue)) {
         alert("Please enter a valid email address (e.g. yourname@example.com)");
         emailInput.focus();
         return;
       }
-  
-      // EmailJS로 전송
+
       emailjs
         .sendForm("service_0cmqs4l", "template_uannx6t", this)
         .then(() => {
